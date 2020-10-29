f8f12cbd23abb69193a141b12a0b5ade1bc28fd1
sthan41 <s.thanusan@hotmail.ch>
2020-10-29 15:00:06 +0100

    structered the code

--- milestone2/bitcoin.py
+++ milestone2/bitcoin.py
@@ -1,6 +1,12 @@
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+
+
 # Import the dataset and encode the date
 df = pd.read_csv('bitcoin.csv')
 df['date'] = pd.to_datetime(df['Timestamp'],unit='s').dt.date
@@ -10,19 +16,18 @@ Real_Price = group['Weighted_Price'].mean()
 prediction_days = 30
 df_train= Real_Price[:len(Real_Price)-prediction_days]
 df_test= Real_Price[len(Real_Price)-prediction_days:]
+
+
 #process data
 training_set=df_train.values
 training_set=np.reshape(training_set, (len(training_set), 1))
-from sklearn.preprocessing import MinMaxScaler
 sc=MinMaxScaler()
 training_set=sc.fit_transform(training_set)
 X_train= training_set[0:len(training_set)-1]
 y_train= training_set[1:len(training_set)]
 X_train= np.reshape(X_train, (len(X_train), 1, 1))
-#importing the keras libraries and packages
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
+
+
 # Initialising the RNN
 regressor = Sequential()
 
@@ -36,6 +41,7 @@ regressor.add(Dense(units = 1))
 regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
 # Fitting the RNN to the Training set
 regressor.fit(X_train, y_train, batch_size = 5, epochs = 100)
+
 # Making the predictions
 test_set = df_test.values
 inputs = np.reshape(test_set, (len(test_set), 1))
@@ -43,6 +49,9 @@ inputs = sc.transform(inputs)
 inputs = np.reshape(inputs, (len(inputs), 1, 1))
 predicted_BTC_price = regressor.predict(inputs)
 predicted_BTC_price = sc.inverse_transform(predicted_BTC_price)
+
+
+
 # Visualising the results
 plt.figure(figsize=(25,15), dpi=50, facecolor='w', edgecolor='k')
 ax = plt.gca()  
