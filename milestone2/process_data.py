c5d3939fcc639095a13f48babe11341393863bcd
sthan41 <s.thanusan@hotmail.ch>
2020-10-30 16:46:30 +0100

    putted all the import commands in it

--- milestone2/process_data.py
+++ milestone2/process_data.py
@@ -7,15 +7,5 @@ X_train= training_set[0:len(training_set)-1]
 y_train= training_set[1:len(training_set)]
 X_train= np.reshape(X_train, (len(X_train), 1, 1))
 
-# Initialising the RNN
-regressor = Sequential()
 
-# Adding the input layer and the LSTM layer
-regressor.add(LSTM(units = 4, activation = 'sigmoid', input_shape = (None, 1)))
-
-# Adding the output layer
-regressor.add(Dense(units = 1))
-
-# Compiling the RNNregressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
-regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
 
